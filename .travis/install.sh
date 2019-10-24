4e2739953e6a789839547e42bdbed8b3bf4d1864
Fernando Godoy <fernandogodoy18@gmail.com>
2019-10-24 02:50:21 -0300

    install sh

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,5 +1,5 @@
 #if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ];
 if [ "$TRAVIS_BRANCH" = "master" ];
 then
-    openssl aes-256-cbc -K $ENCRYPTED_GPG_KEY_ID -iv $ENCRYPTED_GPG_PASSPHRASE -in $ENCRYPTED_GPG_KEY_LOCATION -out $GPG_KEY_LOCATION -d
+    openssl aes-256-cbc -K $ENCRYPTED_GPG_KEY_ID_key -iv $ENCRYPTED_GPG_PASSPHRASE_iv -in $ENCRYPTED_GPG_KEY_LOCATION -out $GPG_KEY_LOCATION -d
 fi
