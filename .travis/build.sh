19b6862604e482479238bd7850b3a0c83dbb9a2f
Fernando Godoy <fernandogodoy18@gmail.com>
2019-10-24 02:34:25 -0300

    Publish config

---
+++ .travis/build.sh
@@ -0,0 +1,9 @@
+chmod u+x gradlew
+ 
+if [ "$TRAVIS_PULL_REQUEST" != "false" ];
+then
+    echo "Skip integration tests in pull request builds"
+    ./gradlew clean build -x integrationTest
+else
+    ./gradlew clean build
+fi
