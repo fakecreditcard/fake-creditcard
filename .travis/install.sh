9e03d9fedfe978c863dc26a24229fb003888667d
Fernando Godoy <fernandogodoy18@gmail.com>
2019-10-24 03:38:33 -0300

    install sh

--- .travis/install.sh
+++ .travis/install.sh
@@ -2,5 +2,5 @@
 if [ "$TRAVIS_BRANCH" = "master" ];
 then
     #openssl aes-256-cbc -K "$encrypted_77f17bf3ee01_key" -iv "$encrypted_77f17bf3ee01_iv" -in "$ENCRYPTED_GPG_KEY_LOCATION" -out "$GPG_KEY_LOCATION" -d
-    openssl enc -aes-256-cbc -d -in $ENCRYPTED_GPG_KEY_LOCATION -out $CLI_DIR/signingkey.asc -base64 -K $encrypted_77f17bf3ee01_key -iv $encrypted_77f17bf3ee01_iv
+    openssl enc -aes-256-cbc -d -in $ENCRYPTED_GPG_KEY_LOCATION -out $GPG_KEY_LOCATION -base64 -K $encrypted_77f17bf3ee01_key -iv $encrypted_77f17bf3ee01_iv
 fi
