6be665958ac4b25c9d5f17e4e53731f2aeb779af
Fernando Godoy <fernandogodoy18@gmail.com>
2019-10-24 09:26:44 -0300

    config gpg

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,6 +1,5 @@
 #if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ];
 if [ "$TRAVIS_BRANCH" = "master" ];
 then
-    #openssl aes-256-cbc -K "$encrypted_77f17bf3ee01_key" -iv "$encrypted_77f17bf3ee01_iv" -in "$ENCRYPTED_GPG_KEY_LOCATION" -out "$GPG_KEY_LOCATION" -d
-    openssl enc -aes-256-cbc -d -in $ENCRYPTED_GPG_KEY_LOCATION -out $GPG_KEY_LOCATION -base64 -K $encrypted_77f17bf3ee01_key -iv $encrypted_77f17bf3ee01_iv
+    openssl aes-256-cbc -K $encrypted_66ba568ec523_key -iv $encrypted_66ba568ec523_iv -in .travis/fakecreditcard.gpg.enc -out .travis/fakecreditcard.gpg -d
 fi
