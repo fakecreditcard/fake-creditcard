3d66b71e842daa558a90d5a9a01e07faa64289be
Fernando Godoy <fernandogodoy18@gmail.com>
2019-11-01 10:20:42 -0300

    Publish

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,6 @@
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ];
 then
-    openssl aes-256-cbc -K $encrypted_66ba568ec523_key -iv $encrypted_66ba568ec523_iv -in ${ENCRYPTED_GPG_KEY_LOCATION} -out ${GPG_KEY_LOCATION} -d
+    #openssl aes-256-cbc -K $encrypted_66ba568ec523_key -iv $encrypted_66ba568ec523_iv -in ${ENCRYPTED_GPG_KEY_LOCATION} -out ${GPG_KEY_LOCATION} -d
 
 	./gradlew uploadArchives \
 		-PossrhUsername=${SONATYPE_USERNAME} \
